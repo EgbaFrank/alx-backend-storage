218edfb3081f71788b5cafcd9391c345218a5598
Egba Frank <egbafrank72@gmail.com>
2024-10-20 01:02:07 +0000

    Add mandatory tasks

---
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -0,0 +1,13 @@
+-- creates a function SafeDiv that divides (and returns)
+DELIMITER |
+
+CREATE FUNCTION SafeDiv(a INT, b INT)
+RETURNS FLOAT
+BEGIN
+	IF b = 0 THEN
+		RETURN 0;
+	END IF;
+	RETURN a / b;
+END |
+
+DELIMITER ;
