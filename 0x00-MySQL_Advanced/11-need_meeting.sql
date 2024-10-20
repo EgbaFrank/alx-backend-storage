010fd51ecb9b516354f076b1a35344b175b959b5
Egba Frank <egbafrank72@gmail.com>
2024-10-20 01:11:29 +0000

    fix datetime compare

--- 0x00-MySQL_Advanced/11-need_meeting.sql
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -3,4 +3,4 @@ CREATE VIEW need_meeting AS
 SELECT name
 FROM students
 WHERE score < 80
-AND (last_meeting IS NULL OR last_meeting > NOW() - INTERVAL 1 MONTH);
+AND (last_meeting IS NULL OR last_meeting < SUBDATE(CURRENT_DATE(), INTERVAL 1 MONTH));
