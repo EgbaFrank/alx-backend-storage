a96444330cb0983581564ab7fbe75d24a12dfa91
Egba Frank <Egbafrank72@gmail.com>
2024-10-24 00:39:45 +0000

    Add type annotations

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -8,12 +8,13 @@ from typing import Callable
 import functools
 
 
-def count_access(func: Callable) -> Callable:
+def cache_response(func: Callable) -> Callable:
     """
     Track how many times a particular URL was accessed
     """
     @functools.wraps(func)
-    def wrapper(*args):
+    def wrapper(args: str) -> str:
+        """Cache wrapper"""
         _redis = redis.Redis()
 
         url = args[0] if args else "unknown"
@@ -28,14 +29,14 @@ def count_access(func: Callable) -> Callable:
         if cached_page:
             return cached_page.decode("utf-8")
 
-        result = func(*args)
+        result = func(args)
         _redis.setex(cache_key, 10, result)
 
         return result
     return wrapper
 
 
-@count_access
+@cache_response
 def get_page(url: str) -> str:
     """Obtain the HTML content of a particular URL"""
     response = requests.get(url)
